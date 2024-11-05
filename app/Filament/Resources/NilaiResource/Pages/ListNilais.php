6f16dba6fda589e9f53fbcc1c4908cf8079d1bc0
Harlan <harlanmuradi@gmail.com>
2024-11-05 16:27:38 +0800

    template

---
+++ app/Filament/Resources/NilaiResource/Pages/ListNilais.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\NilaiResource\Pages;
+
+use App\Filament\Resources\NilaiResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListNilais extends ListRecords
+{
+    protected static string $resource = NilaiResource::class;
+}
