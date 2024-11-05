cf35da720fe4c3fa89719868014c8abc5703f786
Harlan <harlanmuradi@gmail.com>
2024-11-05 09:33:08 +0800

    add panel mahasiswa dan staff

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\MahasiswaPanelProvider::class,
+    App\Providers\Filament\StaffPanelProvider::class,
 ];
