8c1d5c08963fd5c23a79444abe98e360e0ac0f4d
Harlan <harlanmuradi@gmail.com>
2024-11-05 16:05:09 +0800

    primarykey and foreignkey fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,13 +22,13 @@ class JabatansSeeder extends Seeder
     public function run()
     {
         DB::table('jabatans')->insert([
-            ['idjabatan' => 1, 'jabatan' => 'Dosen', 'created_at' => now(), 'updated_at' => now()],
-            ['idjabatan' => 2, 'jabatan' => 'Kepala Program Studi', 'created_at' => now(), 'updated_at' => now()],
-            ['idjabatan' => 3, 'jabatan' => 'Dosen Tamu', 'created_at' => now(), 'updated_at' => now()],
-            ['idjabatan' => 4, 'jabatan' => 'Asisten Dosen', 'created_at' => now(), 'updated_at' => now()],
-            ['idjabatan' => 5, 'jabatan' => 'Pengajar Praktikum', 'created_at' => now(), 'updated_at' => now()],
-            ['idjabatan' => 6, 'jabatan' => 'Koordinator Dosen', 'created_at' => now(), 'updated_at' => now()],
-            ['idjabatan' => 7, 'jabatan' => 'Dosen Senior', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 1, 'jabatan' => 'Dosen', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 2, 'jabatan' => 'Kepala Program Studi', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 3, 'jabatan' => 'Dosen Tamu', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 4, 'jabatan' => 'Asisten Dosen', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 5, 'jabatan' => 'Pengajar Praktikum', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 6, 'jabatan' => 'Koordinator Dosen', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 7, 'jabatan' => 'Dosen Senior', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
@@ -74,12 +74,12 @@ class DospemsSeeder extends Seeder
     public function run()
     {
         DB::table('dospems')->insert([
-            ['id' => 1, 'idjabatan' => 1, 'id' => json_encode(["3", "6", "1", "5"]), 'namadosen' => 'Herfia Romadhoni', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 2, 'idjabatan' => 2, 'id' => json_encode(["1", "2"]), 'namadosen' => 'BIlly Sabela', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 3, 'idjabatan' => 1, 'id' => json_encode(["2", "4", "5"]), 'namadosen' => 'Nina Sari', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 4, 'idjabatan' => 2, 'id' => json_encode(["1", "3", "6"]), 'namadosen' => 'Danu Kurniawan', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 5, 'idjabatan' => 1, 'id' => json_encode(["2", "5", "7"]), 'namadosen' => 'Sari Dewi', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 6, 'idjabatan' => 3, 'id' => json_encode(["8", "9"]), 'namadosen' => 'Eko Prabowo', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 1, 'idjabatan' => 1, 'idmatkul' => json_encode(["3", "6", "1", "5"]), 'namadosen' => 'Herfia Romadhoni', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 2, 'idjabatan' => 2, 'idmatkul' => json_encode(["1", "2"]), 'namadosen' => 'BIlly Sabela', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 3, 'idjabatan' => 1, 'idmatkul' => json_encode(["2", "4", "5"]), 'namadosen' => 'Nina Sari', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 4, 'idjabatan' => 2, 'idmatkul' => json_encode(["1", "3", "6"]), 'namadosen' => 'Danu Kurniawan', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 5, 'idjabatan' => 1, 'idmatkul' => json_encode(["2", "5", "7"]), 'namadosen' => 'Sari Dewi', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 6, 'idjabatan' => 3, 'idmatkul' => json_encode(["8", "9"]), 'namadosen' => 'Eko Prabowo', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
@@ -89,14 +89,14 @@ class MahasiswasSeeder extends Seeder
     public function run()
     {
         DB::table('mahasiswas')->insert([
-            ['id' => 1, 'id' => 2, 'id' => 2, 'namamahasiswa' => 'Ahlul Nazar', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 2, 'id' => 1, 'id' => 1, 'namamahasiswa' => 'Budi Santoso', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 3, 'id' => 3, 'id' => 3, 'namamahasiswa' => 'Cynthia Wulan', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 4, 'id' => 4, 'id' => 4, 'namamahasiswa' => 'Dewi Kinasih', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 5, 'id' => 5, 'id' => 5, 'namamahasiswa' => 'Fahri Amrullah', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 6, 'id' => 6, 'id' => 6, 'namamahasiswa' => 'Gina Indira', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 7, 'id' => 7, 'id' => 1, 'namamahasiswa' => 'Hendra Saputra', 'created_at' => now(), 'updated_at' => now()],
-            ['id' => 8, 'id' => 2, 'id' => 2, 'namamahasiswa' => 'Intan Permatasari', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 1, 'idkelas' => 2, 'iddospem' => 2, 'namamahasiswa' => 'Ahlul Nazar', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 2, 'idkelas' => 1, 'iddospem' => 1, 'namamahasiswa' => 'Budi Santoso', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 3, 'idkelas' => 3, 'iddospem' => 3, 'namamahasiswa' => 'Cynthia Wulan', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 4, 'idkelas' => 4, 'iddospem' => 4, 'namamahasiswa' => 'Dewi Kinasih', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 5, 'idkelas' => 5, 'iddospem' => 5, 'namamahasiswa' => 'Fahri Amrullah', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 6, 'idkelas' => 6, 'iddospem' => 6, 'namamahasiswa' => 'Gina Indira', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 7, 'idkelas' => 7, 'iddospem' => 1, 'namamahasiswa' => 'Hendra Saputra', 'created_at' => now(), 'updated_at' => now()],
+            ['id' => 8, 'idkelas' => 2, 'iddospem' => 2, 'namamahasiswa' => 'Intan Permatasari', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
