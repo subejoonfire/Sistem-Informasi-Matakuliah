38ed3f2306c038ed0153158558a58b9f5b8ed063
Harlan <harlanmuradi@gmail.com>
2024-11-07 09:40:47 +0800

    pdf download linking

--- app/Filament/Resources/NilaiResource/Pages/ListNilais.php
+++ app/Filament/Resources/NilaiResource/Pages/ListNilais.php
@@ -2,8 +2,9 @@
 
 namespace App\Filament\Resources\NilaiResource\Pages;
 
-use App\Filament\Resources\NilaiResource;
+use App\Models\Nilai;
 use Filament\Actions;
+use App\Filament\Resources\NilaiResource;
 use Filament\Resources\Pages\ListRecords;
 
 class ListNilais extends ListRecords
@@ -13,6 +14,11 @@ class ListNilais extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            Actions\Action::make('download')
+                ->url(route('pdf.nilai.download'))
+                ->openUrlInNewTab()
+                ->icon('heroicon-o-document')
+                ->label('Download PDF')
         ];
     }
 }
