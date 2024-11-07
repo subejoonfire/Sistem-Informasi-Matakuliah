4bf312e8f0f4827d321b59433afa2370c868b4e9
Harlan <harlanmuradi@gmail.com>
2024-11-07 13:48:15 +0800

    add delete button

--- app/Filament/Resources/NilaiResource.php
+++ app/Filament/Resources/NilaiResource.php
@@ -114,8 +114,9 @@ class NilaiResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
                 Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
