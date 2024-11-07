2f633f1cc0130474e39969ef0db379b7e3f8e5ce
Harlan <harlanmuradi@gmail.com>
2024-11-07 09:40:58 +0800

    pdf download linking

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('admin/login');
 });
+Route::get('pdf/nilai/download', function () {
+    echo 'halo';
+    // return redirect('admin/login');
+})->name('pdf.nilai.download');
